9a61b6a8dbb32cc611d0bcf1fd68781870e1ee14
Abe <abe.chen723@gmail.com>
2018-11-09 00:08:02 +0800

    [feature] detect if use IDE

--- src/main/scala-2.11/com/cathay/helper/ConfigHelper.scala
+++ src/main/scala-2.11/com/cathay/helper/ConfigHelper.scala
@@ -1,11 +1,17 @@
 package com.cathay.helper
 
 import java.io.File
+import java.lang.management.ManagementFactory
 
 import com.typesafe.config.ConfigFactory
 
 
 trait ConfigHelper {
+
+  private val conPath = ManagementFactory.getRuntimeMXBean().getInputArguments().size() match {
+    case 0 => "conf/api.conf"
+    case _ => "conf/dev/api.conf" // debug mode
+  }
 
   private val conPath = "conf/api.conf"
   private val config = ConfigFactory.parseFile(
